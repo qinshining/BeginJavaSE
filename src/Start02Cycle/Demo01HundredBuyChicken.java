9b0acca993cd245b1d9f001863898346c4cf6243
qinshining <ederqin@qq.com>
2021-03-10 01:17:02 +0800

    fixed buy chicken count not right problem

--- src/Start02Cycle/Demo01HundredBuyChicken.java
+++ src/Start02Cycle/Demo01HundredBuyChicken.java
@@ -2,16 +2,16 @@ package Start02Cycle;
 
 public class Demo01HundredBuyChicken {
     public static void main(String[] args) {
-        final int smallPrice = 1;
-        final int middlePrice = 3;
-        final int bigPrice = 5;
+        final double smallPrice = 1.0 / 3;
+        final double middlePrice = 3;
+        final double bigPrice = 5;
 
-        for (int x = 0; x <= 100; x++) {
-            for (int y = 0; y <= 100; y++) {
-                for (int z = 0; z <= 100; z++) {
-                    if ((x * smallPrice
-                            + y * middlePrice
-                            + z * bigPrice) == 100) {
+        for (int x = 1; x <= 100; x++) {
+            for (int y = 1; y <= 100; y++) {
+                for (int z = 1; z <= 100; z++) {
+                    if ((x + y + z) == 100
+                            && x % 3 == 0
+                            && (x * smallPrice + y * middlePrice + z * bigPrice) == 100) {
                         System.out.println("small:" + x + "\t" + "middle:" + y + "\t" + "big:" + z);
                     }
                 }
